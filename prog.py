230af033b34d3a7c5efbdaf7cb30ef1b20d84e36
Gero-B-Anonuevo <150780703+Gero-B-Anonuevo@users.noreply.github.com>
2024-01-22 16:29:50 +0800

    removing pseudo code, gitbash did not detected the deletion prog.py

--- prog.py
+++ prog.py
@@ -1,11 +1,7 @@
 import tkinter as tk
 from tkinter import simpledialog
 
-#pseudocode
-#accept string
 string_var = tk.simpledialog.askstring("Hello", "Input your sentence: ")
 find_string = tk.simpledialog.askstring("Hello", "What do you want to find? ")
-#use .count() function
 count = string_var.count(find_string)
-#print result
 print(find_string," appeared ",count, " times.")
